81b2aff4815ca0ed77d17552eca1f4923fda1ce8
Paige Hartley <55728245+phartley1@users.noreply.github.com>
2021-11-18 10:32:30 -0500

    Add files via upload

--- components/Popup.js
+++ components/Popup.js
@@ -6,9 +6,6 @@ function Popup(props) {
         <div className = "popup">
             <div className = "inner">
                 <button className = "close" onClick={() => props.setTrigger(false)}>close</button>
-                <a href="https://www.google.com/search?q=boilers&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiPlKe0maL0AhWOhXIEHbZHBdsQ_AUoAnoECAMQBA&biw=683&bih=668&dpr=1.3" target="_blank">
-                    <button>Go!</button>
-                </a>
                 { props.children }
             </div>
         </div>
